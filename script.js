dc16be0a72dbe40aec9cf2fa93397a4df7abd3db
KidVertido <breno.vandoski.silva@escola.pr.gov.br>
2025-09-12 09:05:05 -0300

    ok

---
+++ script.js
@@ -0,0 +1,31 @@
+const avanca = document.querySelectorAll(".btn-proximo");
+const reiniciarBtn = document.getElementById("btn-reiniciar");
+
+avanca.foraEach(button => {
+      button.addEventListerner("click", function(){
+          const atual = document.querySelector(".ativo");
+          const proximo = "passo-"+this.getAttribute("data-proximo");
+
+
+
+
+
+
+
+
+      })
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+)
